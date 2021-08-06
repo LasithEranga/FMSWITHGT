a8e5741b9aff3382eab9d222ce67c900f58d935a
LasithEranga <lasitheranga1@gmail.com>
2021-08-06 12:55:52 +0530

    Database update

--- fmsdb.sql
+++ fmsdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Aug 03, 2021 at 10:34 AM
+-- Generation Time: Aug 06, 2021 at 07:24 AM
 -- Server version: 5.7.31
 -- PHP Version: 7.3.21
 
@@ -44,7 +44,8 @@ CREATE TABLE IF NOT EXISTS `driver` (
 --
 
 INSERT INTO `driver` (`nic`, `fname`, `lname`, `full_name`, `email`, `contact_no`, `address`) VALUES
-('1', 'lasith', 'eranda', 'lasith eranda', 'lasith@fmail.com', '0770543255', 'skndkjnsd');
+('1', 'lasith', 'eranda', 'lasith eranda', 'lasith@fmail.com', '0770543255', 'skndkjnsd'),
+('56456456456', 'hjbhj', 'hjbhj', 'hjbuhvgvghvghvgvghvgh\r\nhvghvghvghvgh\r\n', 'jbnhjbnhj', '0770325244', 'njjkbnkjhbghvgjvghvhgjv');
 
 -- --------------------------------------------------------
 
@@ -57,36 +58,38 @@ CREATE TABLE IF NOT EXISTS `fine_receipt` (
   `Ref_No` bigint(20) NOT NULL AUTO_INCREMENT,
   `Date` date NOT NULL,
   `Amount` mediumint(9) NOT NULL,
+  `vehicle` varchar(50) NOT NULL,
   `State` tinyint(4) NOT NULL,
   `driver_nic` varchar(12) NOT NULL,
-  `officer_id` smallint(5) NOT NULL,
+  `officer_id` varchar(5) NOT NULL,
   PRIMARY KEY (`Ref_No`),
   KEY `driver_nic` (`driver_nic`),
   KEY `officer_id` (`officer_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `fine_receipt`
 --
 
-INSERT INTO `fine_receipt` (`Ref_No`, `Date`, `Amount`, `State`, `driver_nic`, `officer_id`) VALUES
-(2, '2021-07-30', 200, 3, '1', 2),
-(3, '2021-07-24', 545, 5, '1', 2),
-(4, '2021-07-25', 54, 5, '1', 2),
-(5, '2021-07-20', 458, 5, '1', 2),
-(6, '2021-07-12', 218, 2, '1', 2),
-(7, '2020-03-19', 545, 5, '1', 2),
-(8, '2021-07-31', 654, 5, '1', 2),
-(9, '2021-07-30', 654, 5, '1', 2),
-(10, '2021-07-29', 654, 5, '1', 2),
-(11, '2021-07-28', 654, 5, '1', 2),
-(12, '2021-07-27', 654, 5, '1', 2),
-(13, '2021-07-26', 654, 5, '1', 2),
-(14, '2021-07-25', 654, 5, '1', 2),
-(15, '2021-07-24', 654, 5, '1', 2),
-(16, '2021-07-23', 654, 5, '1', 2),
-(17, '2021-07-23', 654, 5, '1', 2),
-(18, '2021-07-23', 658, 5, '1', 2);
+INSERT INTO `fine_receipt` (`Ref_No`, `Date`, `Amount`, `vehicle`, `State`, `driver_nic`, `officer_id`) VALUES
+(2, '2021-07-30', 200, 'car', 3, '1', '2'),
+(3, '2021-07-24', 545, 'motor bike', 5, '1', '2'),
+(4, '2021-07-25', 54, 'lorry', 5, '1', '2'),
+(5, '2021-07-20', 458, 'lorry', 5, '1', '2'),
+(6, '2021-07-12', 218, 'car', 2, '1', '2'),
+(7, '2020-03-19', 545, 'car', 5, '1', '2'),
+(8, '2021-07-31', 654, 'lorry', 5, '1', '2'),
+(9, '2021-07-30', 654, 'motor bike', 5, '1', '2'),
+(10, '2021-07-29', 654, 'lorry', 5, '1', '2'),
+(11, '2021-07-28', 654, 'car', 5, '1', '2'),
+(12, '2021-07-27', 654, 'lorry', 5, '1', '2'),
+(13, '2021-07-26', 654, 'car', 5, '1', '2'),
+(14, '2021-07-25', 654, 'lorry', 5, '1', '2'),
+(15, '2021-07-24', 654, 'car', 5, '1', '2'),
+(16, '2021-07-23', 654, 'car', 5, '1', '2'),
+(17, '2021-07-23', 654, 'lorry', 5, '1', '2'),
+(18, '2021-07-23', 658, 'car', 5, '1', '2'),
+(19, '2021-08-05', 5647, 'car', 3, '1', '35151');
 
 -- --------------------------------------------------------
 
@@ -116,14 +119,15 @@ CREATE TABLE IF NOT EXISTS `payment` (
   PRIMARY KEY (`id`),
   KEY `driver_nic` (`driver_nic`),
   KEY `fince_receipt_Ref` (`fince_receipt_Ref`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `payment`
 --
 
 INSERT INTO `payment` (`id`, `date`, `amount`, `driver_nic`, `fince_receipt_Ref`) VALUES
-(1, '2021-07-30', 1200, '1', 2);
+(1, '2021-07-30', 1200, '1', 2),
+(2, '2021-08-05', 327, '1', 7);
 
 -- --------------------------------------------------------
 
@@ -176,13 +180,13 @@ CREATE TABLE IF NOT EXISTS `rules_broken` (
 
 DROP TABLE IF EXISTS `traffic_police_officer`;
 CREATE TABLE IF NOT EXISTS `traffic_police_officer` (
-  `police_id` smallint(5) NOT NULL,
+  `police_id` varchar(5) NOT NULL,
   `fname` varchar(50) NOT NULL,
   `lname` varchar(50) NOT NULL,
   `full_name` varchar(100) NOT NULL,
   `email` varchar(50) NOT NULL,
   `nic` varchar(12) NOT NULL,
-  `contact_no` char(10) NOT NULL,
+  `contact_no` char(21) NOT NULL,
   `post` varchar(50) NOT NULL,
   `address` varchar(150) NOT NULL,
   PRIMARY KEY (`police_id`)
@@ -193,7 +197,9 @@ CREATE TABLE IF NOT EXISTS `traffic_police_officer` (
 --
 
 INSERT INTO `traffic_police_officer` (`police_id`, `fname`, `lname`, `full_name`, `email`, `nic`, `contact_no`, `post`, `address`) VALUES
-(2, 'fname', 'lname', 'full name', 'email', '0770325244', '0770325244', 'sfnjksdnbj', 'kjnvsdjkvn');
+('2', 'fname', 'lname', 'full name', 'email', '0770325244', '0770325244', 'sfnjksdnbj', 'kjnvsdjkvn'),
+('35151', 'Lasith', 'jksdhfj', 'jkfndsjkn', 'fjksnd@shd', '990811130V', '(+94) 77 - 054 - 3231', 'jksdhfjk', ''),
+('45564', 'Lasith', 'dkjsnfj', 'kfjdsnjkfn', 'ksjk@jdj', '9905456454V', '(+94) 77 - 535 - 4546', 'jksnsdjkf', 'ksjdknk');
 
 -- --------------------------------------------------------
 
